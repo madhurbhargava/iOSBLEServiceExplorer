3298e7cf59f5882064a3c13294c395b925651a58
Madhur Bhargava <madhur.bhargava333@gmail.com>
2017-11-06 09:56:42 +0100

    Fix for API Misuse Error API MISUSE. #1

--- iOSBLEServiceExplorer/iOSBLEServiceExplorer/ViewController.swift
+++ iOSBLEServiceExplorer/iOSBLEServiceExplorer/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
 
     @IBOutlet weak var textView: UITextView!
     var manager:CBCentralManager!
+    var devicePeripherals = [CBPeripheral]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,36 +34,55 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
             .object(forKey: CBAdvertisementDataLocalNameKey)
             as? NSString
         
-        if device != nil {
+        if device != nil && !devicePeripherals.contains(peripheral) {
+            devicePeripherals.append(peripheral)
             textView.text.append("===Device Name:\(device) \n")
             manager.connect(peripheral, options: nil)
         }
     }
     
     public func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        peripheral.delegate = self
-        peripheral.discoverServices(nil)
+        if(devicePeripherals.contains(peripheral)) {
+            let index = devicePeripherals.index(of: peripheral)
+            if let index = index {
+                let discoveredPeripheral = devicePeripherals[index]
+                discoveredPeripheral.delegate = self
+                discoveredPeripheral.discoverServices(nil)
+            }
+        }
     }
     
     public func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-        if let services = peripheral.services {
-            for service in services {
-                print("Service:: \(service)")
-                textView.text.append("Service found:\(service.uuid.uuidString) for device \(peripheral.name) \n")
-                textView.text.append("\n")
-                peripheral.discoverCharacteristics(nil, for: service)
+        if(devicePeripherals.contains(peripheral)) {
+            let index = devicePeripherals.index(of: peripheral)
+            if let index = index {
+                let discoveredPeripheral = devicePeripherals[index]
+                if let services = discoveredPeripheral.services {
+                    for service in services {
+                        print("Service:: \(service)")
+                        textView.text.append("Service found:\(service.uuid.uuidString) for device \(discoveredPeripheral.name) \n")
+                        textView.text.append("\n")
+                        peripheral.discoverCharacteristics(nil, for: service)
+                    }
+                }
             }
         }
     }
     
     public func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-        if let characteristics = service.characteristics {
-            for characteristic in characteristics {
-                textView.text.append("Characteristic found:\(characteristic.uuid.uuidString) for Service:\(service.uuid.uuidString) for device \(peripheral.name) \n")
-                textView.text.append("\n")
-                print(characteristic.uuid.uuidString)
+        if(devicePeripherals.contains(peripheral)) {
+            let index = devicePeripherals.index(of: peripheral)
+            if let index = index {
+                let discoveredPeripheral = devicePeripherals[index]
+                if let characteristics = service.characteristics {
+                    for characteristic in characteristics {
+                        textView.text.append("Characteristic found:\(characteristic.uuid.uuidString) for Service:\(service.uuid.uuidString) for device \(discoveredPeripheral.name) \n")
+                        textView.text.append("\n")
+                        print(characteristic.uuid.uuidString)
+                    }
+                    
+                }
             }
-            
         }
     }
 }
